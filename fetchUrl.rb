b8909884dcf95eb0f01d7dd37bb61a2a16b15a0a
Mateusz Hinc <mateusz.hinc95@gmail.com>
2019-11-12 21:10:50 +0100

    initial commit

---
+++ fetchUrl.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'uri'
+require 'cgi'
+
+def fetchUrl(url, log)
+    begin
+        log.debug "Trying to fetch #{url}"
+
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response uri
+
+        log.debug 'Fetched successfully!'
+        return CGI.unescape response.body        
+    rescue SocketError => error
+        log.fatal 'Could not connect to URL!'
+        exit 1
+    end
+end
+
+class String
+
+    def tryEncode(log) 
+        rawBody = self
+        log.debug 'Trying to determine encoding'
+
+        encoding = 'UTF-8'
+
+        match = rawBody.match /charset="?(?<determined>.*)"/i
+        if (match) 
+            encoding = match[:determined]
+            log.debug "Determined encoding: #{encoding}"
+            rawBody = rawBody.force_encoding(encoding)
+        else
+            log.debug "Could not determine encoding, using #{encoding} by default"
+        end
+
+        return rawBody.encode 'UTF-8'
+    end
+
+    def unHTMLize(log)
+        rawBody = self
+        if rawBody.match /<body.?>(?<body>.*)<\/body.?>/im
+            log.info "The HTML has a valid <body> tag. Trimming to <body> only."
+            match = /<body.?>(?<body>.*)<\/body.?>/im
+            rawBody = match[:body]
+        else
+            log.debug "The HTML has no valid <body> tag. Using the whole markup."
+        end
+
+
+
+        return rawBody.gsub /<(.|\n)*?>/, ''
+    end
+
+    def removeEmptyLines(log)
+        rawBody = self
+
+        return rawBody.gsub /^[^\w]*$/, ''
+    end
+
+end
---
+++ mathin.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require 'getoptlong'
+require_relative 'fetchUrl'
+require 'logger'
+
+log = Logger.new STDOUT
+
+opts = GetoptLong.new(
+[ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+[ '--verbose', '-v', GetoptLong::NO_ARGUMENT ],
+[ '--silent', '-s', GetoptLong::REQUIRED_ARGUMENT ])
+
+HELPMESSAGE = """
+Command: mathin.rb [options] URL
+Params:
+    URL         -       the URL of the page to be fetched
+Options:
+    --help      -h      Print help (this text) and exit.
+    --verbose   -v      Be more verbose about what you're doing.
+    --silent    -s      Don't log anything at all.
+"""
+
+verbose = false
+
+opts.each do |opt, arg|
+    case opt
+        when '--help' then print HELPMESSAGE; exit 0
+        when '--verbose' then log.level = :debug
+        when '--silent' then log.level = :Unknown
+        # when '--tree' then $tree = arg
+        else print "Unknown option (#{opt}). Ignored"
+    end
+end
+
+puts fetchUrl('https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna', log)
+    .tryEncode(log)
+    .unHTMLize(log)
+    .removeEmptyLines(log)
+puts 'end'
